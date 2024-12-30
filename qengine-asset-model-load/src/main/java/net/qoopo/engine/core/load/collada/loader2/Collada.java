c89f4f6ddc1e0edc6b9612de3c758aa1fbd5647f
Alberto <alberto.garcia@qoopo.net>
2024-12-30 09:38:01 -0500

    add export model load

---
+++ qengine-asset-model-load/src/main/java/net/qoopo/engine/core/load/collada/loader2/Collada.java
@@ -0,0 +1,19 @@
+package net.qoopo.engine.core.load.collada.loader2;
+
+/**
+ * Collada class.
+ * 
+ * @author Leonardo Ono (ono.leo80@gmail.com)
+ */
+public class Collada {
+    
+    private final Libraries libraries = new Libraries();
+
+    public Collada() {
+    }
+
+    public Libraries getLibraries() {
+        return libraries;
+    }
+    
+}
